318bd5d8a8f3a5a2dbb9e1f6535a70ec94b7606f
Pranit Rathod <pranitrathodsocaial@gmail.com>
2024-10-05 17:09:03 +0530

    Added Spring boot POJO class

---
+++ POJO.java
@@ -0,0 +1,27 @@
+public class POJO {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    String name;
+    int age;
+
+    @Override
+    public String toString() {
+        return "POJO{name='" + name + "', age=" + age + "}";
+    }
+
+}
